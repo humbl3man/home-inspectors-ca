92aa7457a5336b4b36e52e93444ec90ca0f71394
Konstantin M <konstantindev925@gmail.com>
2018-04-06 11:34:12 -0700

    add static path map config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass();
+module.exports = withSass({
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/services': { page: '/services' },
+      '/about-us': { page: '/about-us' },
+      '/contact-us': { page: '/contact-us' },
+      '/faq': { page: '/faq' }
+    };
+  }
+});
