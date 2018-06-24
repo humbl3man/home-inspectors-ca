1c2617b677e14740b2bcc6c00cbf01c7c36788c8
humbl3man <konstantindev925@gmail.com>
2018-06-24 13:14:34 -0700

    add phone number in header

--- components/Header.js
+++ components/Header.js
@@ -66,6 +66,9 @@ class Header extends Component {
             </Link>
           );
         })}
+        <a href="tel:+18582472631" className="btn btn-outline-light call-now">
+          <i className="fa fa-phone" /> Call Now
+        </a>
       </nav>
     );
   }
